6b729a9e8bdee173671005d99d362ec1c0bd012f
Chike Chris <cchike2017@gmail.com>
2019-07-26 12:10:35 -0400

    used axios to fetch data.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard({ character }) {
+  const { name, status, species } = character;
+  return (
+    <div className="character-card">
+      <Link to={`/characters/${character.id}`}>
+        <h2>{name}</h2>
+      </Link>
+      <div className="species-status">
+        <p>
+          {species} {status}
+        </p>
+      </div>
+    </div>
+  );
 }
