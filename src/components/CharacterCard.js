3d007c88ec131f4b3fc8777464b3502cff17b641
Chike Chris <cchike2017@gmail.com>
2019-07-26 12:33:15 -0400

    rendered charactLists. installed styledComponents

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+
+import styled from "styled-components";
+
+const DivCharacters = styled.div`
+  font-family: "Serif", "Georgia";
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+const ImageCharacter = styled.img`
+  width: 250px;
+  border-radius: 20px;
+`;
 
 export default function CharacterCard({ character }) {
-  const { name, status, species } = character;
   return (
-    <div className="character-card">
-      <Link to={`/characters/${character.id}`}>
-        <h2>{name}</h2>
-      </Link>
-      <div className="species-status">
-        <p>
-          {species} {status}
-        </p>
-      </div>
-    </div>
+    <DivCharacters>
+      <ImageCharacter src={character.image} alt={character.name} />
+      <h3>{character.name}</h3>
+      <h4>
+        {character.species} {character.status}
+      </h4>
+      <p>Location: {character.location.name}</p>
+      <p>Origin: {character.origin.name}</p>
+      {console.log(character)}
+    </DivCharacters>
   );
 }
