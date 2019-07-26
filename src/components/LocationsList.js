81041bb4d063cc40f65239bd1c94884b8205ad24
Chike Chris <cchike2017@gmail.com>
2019-07-26 12:54:54 -0400

    fetctd data using axios, built out locationList func.

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard";
 
 export default function LocationsList() {
+  const [locations, setLocations] = useState([]);
 
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/location/`)
+      .then(res => {
+        setCharacters([...res.data.results]);
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {locations.map(location, id => (
+        <LocationCard location={location} key={id} />
+      ))}
+    </div>
+  );
 }
